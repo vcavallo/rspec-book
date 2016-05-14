4016c00569d7e9e43ac50875d8f2132be2a4c99d
vinney cavallo <vcavallo@gmail.com>
2016-05-14 13:27:07 -0400

    Post-Marker extraction

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -11,27 +11,8 @@ module Codebreaker
     end
 
     def guess(guess)
-      @output_method.puts '+'*exact_match_count(guess) + '-'*number_match_count(guess)
-    end
-
-    def exact_match_count(guess)
-      (0..3).inject(0) do |count, index|
-        count + (exact_match?(guess, index) ? 1 : 0)
-      end
-    end
-
-    def number_match_count(guess)
-      (0..3).inject(0) do |count, index|
-        count + (number_match?(guess, index) ? 1 : 0)
-      end
-    end
-
-    def exact_match?(guess, index)
-      guess[index] == @secret[index]
-    end
-
-    def number_match?(guess, index)
-      @secret.include?(guess[index]) && !exact_match?(guess, index)
+      marker = Marker.new(@secret)
+      @output_method.puts '+'*marker.exact_match_count(guess) + '-'*marker.number_match_count(guess)
     end
   end
 end
