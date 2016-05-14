5f9c11503d7a648defc324cbc3e2b5e349c8aeed
vinney cavallo <vcavallo@gmail.com>
2016-05-14 13:26:27 -0400

    Prior to Marker extraction

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -5,11 +5,60 @@ module Codebreaker
     end
 
     def start(secret)
+      @secret = secret
       @output_method.puts('Welcome to Codebreaker!')
       @output_method.puts('Enter guess:')
     end
 
     def guess(guess)
+      @output_method.puts '+'*exact_match_count(guess) + '-'*number_match_count(guess)
+    end
+
+    def exact_match_count(guess)
+      (0..3).inject(0) do |count, index|
+        count + (exact_match?(guess, index) ? 1 : 0)
+      end
+    end
+
+    def number_match_count(guess)
+      (0..3).inject(0) do |count, index|
+        count + (number_match?(guess, index) ? 1 : 0)
+      end
+    end
+
+    def exact_match?(guess, index)
+      guess[index] == @secret[index]
+    end
+
+    def number_match?(guess, index)
+      @secret.include?(guess[index]) && !exact_match?(guess, index)
     end
   end
+end
+
+class Marker
+  def initialize(secret)
+    @secret = secret
+  end
+
+  def exact_match_count(guess)
+    (0..3).inject(0) do |count, index|
+      count + (exact_match?(guess, index) ? 1 : 0)
+    end
+  end
+
+  def number_match_count(guess)
+    (0..3).inject(0) do |count, index|
+      count + (number_match?(guess, index) ? 1 : 0)
+    end
+  end
+
+  def exact_match?(guess, index)
+    guess[index] == @secret[index]
+  end
+
+  def number_match?(guess, index)
+    @secret.include?(guess[index]) && !exact_match?(guess, index)
+  end
+
 end
--- codebreaker/spec/codebreaker/game_spec.rb
+++ codebreaker/spec/codebreaker/game_spec.rb
@@ -25,6 +25,34 @@ module Codebreaker
           game.guess('5555')
         end
       end
+      context "with 1 number match" do
+        it "sends a mark with '-'" do
+          game.start('1234')
+          expect(output).to receive(:puts).with('-')
+          game.guess('2555')
+        end
+      end
+      context "with 2 number matches" do
+        it "sends a mark with '--'" do
+          game.start('1234')
+          expect(output).to receive(:puts).with('--')
+          game.guess('2355')
+        end
+      end
+      context "with 1 exact match" do
+        it "sends a mark with '+'" do
+          game.start('1234')
+          expect(output).to receive(:puts).with('+')
+          game.guess('1555')
+        end
+      end
+      context "with 1 number match and 1 exact match (in that order)" do
+        it "sends a mark with '+-'" do
+          game.start('1234')
+          expect(output).to receive(:puts).with('+-')
+          game.guess('2535')
+        end
+      end
     end
   end
 end
